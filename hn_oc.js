9b1984ff391bb344d636b772f7b6faccf6902598
Michael Lai <themichaellai@gmail.com>
2013-09-14 22:52:32 -0400

    Removed console.logs

--- hn_oc.js
+++ hn_oc.js
@@ -10,14 +10,12 @@ var openBoth = function() {
 for (i = 1; i < link_rows.length - 3; i++) {
   if (i % 3 == 1) {
     main_link = link_rows[i].querySelector('.title a').getAttribute('href');
-    console.log(main_link);
   }
   else if (i % 3 == 2) {
     var comment_link = link_rows[i].querySelectorAll('a')[1];
     // there is no comment link if it is a job listing
     if (comment_link && main_link != comment_link) {
       comment_link = comment_link.getAttribute('href');
-      console.log(comment_link);
       var link = document.createElement('a');
       link.innerHTML = '[l+c]';
       link.setAttribute('class', 'comment-main-link');
